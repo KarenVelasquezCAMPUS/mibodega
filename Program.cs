84cf1fba27ca6861dd5ba5905690246c2bb52a3b
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-07-11 12:55:33 -0500

    feat: :white_check_mark: migracion completada
    funciona bros, si sé cSharp

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MiBodegaContext>(options =>
+{
+    string ? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
